46ccafdf313a2ac6744cae8909dd356659900fa8
anil <sharuk.anil07@gmail.com>
2022-07-06 23:07:32 +0530

    Anil

---
+++ indent.py
@@ -0,0 +1,6 @@
+print("hello world")
+if 3>1 :  #chrecks condition
+    print("greater")
+    print("lesser")
+
+print("athis is indentaion")
---
+++ special_char.py
@@ -0,0 +1,1 @@
+print("welcome to python scripts and \nwe are learning sripts")
